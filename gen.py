b9aa0fe1a34a6d944384b837325b6b24eb578cad
mstepanek0301 <mstepanek0301@gmail.com>
2024-02-13 23:18:10 +0100

    Initial commit

---
+++ gen.py
@@ -0,0 +1,17 @@
+from random import randrange
+
+def gen(total):
+    split_1, split_2 = sorted((
+        randrange(total), randrange(total)
+    ))
+
+    r = split_1
+    g = split_2 - split_1
+    b = total - split_2
+
+    return r, g, b
+
+print('[' + ','.join([
+    '[' + ','.join(map(str, gen(100))) + ']'
+    for _ in range(5)
+]) + ']')
